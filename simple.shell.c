5ef16cb80d8ce231c30dc52e75c0449b64b9f0a8
SalehFulayyih <saleh.5050@hotmail.com>
2025-04-18 10:38:00 +0000

    Add authors file with contributor details

--- simple.shell.c
+++ simple.shell.c
@@ -26,58 +26,58 @@ int main(int argc, char **argv, char **environ)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("$ "); /*printing our prompt symbol1*/
-		
-		user_input = getline(&line, &len,stdin);/*reading command lines form the user*/
+		/*reading command lines form the user*/
+		user_input = getline(&line, &len, stdin);
 		if (user_input == -1)
 		{
 			if (isatty(STDIN_FILENO))
-	printf("\n");
-	break;
-}
-clean = trim_spaces(line);
-line_cpy = malloc(sizeof(char) * strlen(clean) + 1);
-if (line_cpy == NULL)
-	return (-1);
-strncpy(line_cpy, clean, strlen(clean) + 1);
-free(clean);
-if (line_cpy[0] == '\0')
-{
-	free(line_cpy);
-	continue;
-}
-PATH_str = getevn("PATH"); 
-PATH = strtok(PATH_str, ":");
-if (access(PATH, X_OK) != 0)
-{
-	perror("command: ");
-	return (-1);
-}
-pid = fork();
-if (pid == -1)
-{
-	perror("fork");
-	free(line_cpy);
-	exit(EXIT_FAILURE);
-}
-if (pid == 0)
-{
-	token = strtok(line_cpy, " ");
-	while (token != NULL)
-	{
-		arguments[index++] = token;
-		token = strtok(NULL, " ");
+				printf("\n");
+			break;
+		}
+		clean = trim_spaces(line);
+		line_cpy = malloc(sizeof(char) * strlen(clean) + 1);
+		if (line_cpy == NULL)
+			return (-1);
+		strncpy(line_cpy, clean, strlen(clean) + 1);
+		free(clean);
+		if (line_cpy[0] == '\0')
+		{
+			free(line_cpy);
+			continue;
+		}
+		PATH_str = getevn("PATH");
+		PATH = strtok(PATH_str, ":");
+		if (access(PATH, X_OK) != 0)
+		{
+			perror("command: ");
+			return (-1);
+		}
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			free(line_cpy);
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			token = strtok(line_cpy, " ");
+			while (token != NULL)
+			{
+				arguments[index++] = token;
+				oken = strtok(NULL, " ");
+			}
+			arguments[index] = NULL;
+			execve(PATH, arguments, environ);
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+		if (pid > 0)
+		{
+			wait(&status);
+			free(line_cpy);
+		}
 	}
-	arguments[index] = NULL;
-        execve(PATH, arguments, environ);
-	perror("execvp");
-    exit(EXIT_FAILURE);
-}
-if (pid > 0)
-{
-	wait(&status);
-	free(line_cpy);
-}
-}
-free(line);
-return (0);
+	free(line);
+	return (0);
 }
